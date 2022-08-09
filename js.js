0e3cd1b3fced54236ed71de32fa2ce85f4584dd8
nick <nicholasjpenaranda@gmail.com>
2022-08-09 15:47:10 -0500

    Add player turns and restart button on win

--- js.js
+++ js.js
@@ -1,6 +1,46 @@
 const game = (() => {
+    //private
     let _turn = true;
-    const winCheck = (board) => {
+    const _retrybtn = document.querySelector("#retrybtn")
+    const _container = document.querySelector(".container");
+    const _buttons = document.querySelector(".buttons");
+    const _retryP = document.querySelector("#retry");
+    const _start = document.querySelector("#start");
+    const _mess = document.querySelector(".mess");
+    let _gameboard = [null,null,null,null,null,null,null,null,null];
+    let _playerOne;
+    let _playerTwo;
+    let _win = null;
+
+    _buttons.addEventListener("click", () =>{
+        _container.style.display = "grid";
+        document.querySelector(".info").style.display = "none";
+        _buttons.style.display = "none";
+    });
+    
+    _start.addEventListener("click", () => {
+        _playerOne = player(document.querySelector("#p1").value);
+        _playerTwo = player(document.querySelector("#p2").value);
+        _mess.innerHTML = `<p>Its ${_playerOne.name}'s Turn.`;
+    });
+
+    retrybtn.addEventListener("click", () => {
+        _gameboard = [null,null,null,null,null,null,null,null,null];
+        _win = null;
+        _playerOne = null;
+        _playerTwo = null;
+        _turn = true;
+        _mess.innerHTML = ``;
+        document.querySelector("#p1").value = "";
+        document.querySelector("#p2").value = "";
+        _container.style.display = "none";
+        document.querySelector(".info").style.display = "block";
+        _buttons.style.display = "block";
+        _retryP.style.display = "none";
+        displayController();
+    });
+    
+    const _winCheck = (board) => {
         let wins =  [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]];
         for(let [a, b, c] of wins) {
             if(board[a] && board[a] === board[b] && board[a] === board[c]) {
@@ -9,45 +49,51 @@ const game = (() => {
         }
         return;
     }
-    let gameboard = [null,null,null,null,null,null,null,null,null];
-    const container = document.querySelector(".container");
+
+
+
+        //public
     const displayController = () => {
-        while(container.childNodes.length >  0) {
-            container.removeChild(container.lastChild);
+        while(_container.childNodes.length >  0) {
+            _container.removeChild(_container.lastChild);
             };
-        gameboard.forEach((x, index) => {
+        _gameboard.forEach((x, index) => {
             gameToHtml(x, index);
         });
         function gameToHtml(xOrO, index) {
-            _index = index
             const div = document.createElement("div");
             div.innerText = xOrO;
-            container.appendChild(div)
+            _container.appendChild(div)
             div.addEventListener('click', () => {
-                if((gameboard[index] === "X") || (gameboard[index] === "O")) {
+                if((_gameboard[index] === "X") || (_gameboard[index] === "O")) {
                     return;
                 };
-                if(_turn === null) return;
-                gameboard.splice(index, 1, _turn ? "X" : "O");
+                if(_turn === null) return;      //hacky way to stop the game after win
+                _gameboard.splice(index, 1, _turn ? "X" : "O");
                 _turn = _turn !== true;
+                _mess.innerHTML = `<p>Its ${_turn ? _playerOne.name : _playerTwo.name}'s Turn.`;
                 game.displayController();
-                let win = game.winCheck(gameboard);
-                if(win) {
+                _win = _winCheck(_gameboard)
+                if(!_gameboard.some(x => x === null)) {
+                    _mess.innerHTML = `<p>Game Tied</p>`
+                    _retryP.style.display = "block";
                     _turn = null;
-                    document.querySelector(".mess").innerHTML = `<p>${win} wins!</p>`;
+                };
+                if(_win) {
+                    _mess.innerHTML = `<p>${_turn ? _playerTwo.name : _playerOne.name} wins!</p>`;
+                    _retryP.style.display = "block";
+                    _turn = null;
                 };
               });
         }
     }
     return {
-        gameboard,
         displayController,
-        winCheck,
     }
 })();
 
-const Player = (name) => {
-    this.name = name;
-}
+const player = (name) => {
+    return {name,};
+};
 
 game.displayController();
